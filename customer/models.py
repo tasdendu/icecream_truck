4c613bb5b328b7371db8549ee0559392ac219e7a
Tashi Dendup <tashi.dendup@dcpl.bt>
2022-09-09 10:21:54 +0600

    Create model attributes

--- customer/models.py
+++ customer/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+from truck.models import Truck
 
 # Create your models here.
+
+
+class Customer(User, models.Model):
+    phone = models.CharField(max_length=100, unique=True)
+    truck = models.ForeignKey(Truck, related_name='truck')
+    REQUIRED_FIELDS = ['username', 'phone', 'first_name', 'last_name', 'email']
+
+    def get_username(self) -> str:
+        return super().get_username()
--- order/models.py
+++ order/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from customer.models import Customer
+from item.models import Item
 
 # Create your models here.
+
+
+class Order(models.Model):
+    customer = models.ForeignKey(Customer)
+    order_date = models.DateTimeField(auto_now_add=True, blank=False)
+
+    class Meta:
+        ordering = ('order_date',)
+
+
+class LineItem(models.Model):
+    order = models.ForeignKey(Order)
+    item = models.ForeignKey(Item)
+    quantity = models.IntegerField(default=0)
+    price = models.FloatField(default=0.0)
